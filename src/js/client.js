62ace8188e90373db64cb4d9bfa6156a4e8f449c
kuriken121227 <kuri12ken27@gmail.com>
2023-11-09 22:07:46 +0900

    [add]初回コミット

---
+++ src/js/client.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <h1>Welcome!</h1>
+        );
+    }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
